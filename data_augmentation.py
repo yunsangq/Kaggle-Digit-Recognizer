eb7f61f3b949e9f303b095a2dd485daecb5f3715
yunsangq <yunsangq@gmail.com>
2017-08-21 15:19:37 +0900

    587

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,21 +1,112 @@
+import pandas as pd
+from torch import np # Torch wrapper for Numpy
+
 import os
-import matplotlib.pyplot as plt
-import torch as th
-from torchvision import datasets
+from PIL import Image
+
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+TRAIN_DATA = 'train.csv'
+
+'''
+train = pd.read_csv('train.csv')
+Y_train = train.ix[:, 0].values.astype('int32')
+X_train = (train.ix[:, 1:].values).astype('float32')
+X_test = (pd.read_csv('test.csv').values).astype('float32')
+'''
+
+class KaggleDataset(Dataset):
+    """Dataset wrapping images and target labels for Kaggle.
+
+    Arguments:
+        A CSV file path
+        Path to image folder
+        Extension of images
+        PIL transforms
+    """
+
+    def __init__(self, csv_path, transform=None):
+        tmp_df = pd.read_csv(csv_path)
+
+        self.transform = transform
+
+        self.X_train = (tmp_df.ix[:, 1:].values).astype('float32')
+        scale = np.max(self.X_train)
+        self.X_train /= scale
+        self.X_train = self.X_train.reshape(42000, 28, 28).astype('uint8')
+        self.y_train = tmp_df.ix[:, 0].values.astype('int64').reshape(42000, 1)
+        # self.lb = LabelBinarizer()
+        # self.tmp = self.lb.fit(range(max(self.y_train)+1))
+        # self.y_train = self.lb.transform(self.y_train)
+
+    def __getitem__(self, index):
+        img = Image.fromarray(self.X_train[index])
+        if self.transform is not None:
+            img = self.transform(img)
+
+        label = torch.from_numpy(self.y_train[index])
+        return img, label
+
+    def __len__(self):
+        return len(self.X_train)
+
+transformations = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor()
+])
+
+dset_train = KaggleDataset(TRAIN_DATA, transformations)
+
+train_loader = DataLoader(dset_train,
+                          batch_size=128,
+                          shuffle=True,
+                          num_workers=4)
 
 
-# Change this to where you want to save the data
-SAVE_DIR = os.path.expanduser('data/MNIST/')
-# train data
-mnist_train = datasets.MNIST(SAVE_DIR, train=True, download=True)
-x_train_mnist, y_train_mnist = mnist_train.train_data.type(th.FloatTensor), mnist_train.train_labels
-# test data
-mnist_test = datasets.MNIST(SAVE_DIR, train=False, download=True)
-x_test_mnist, y_test_mnist = mnist_test.test_data.type(th.FloatTensor), mnist_test.test_labels
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(1600, 256)
+        self.fc2 = nn.Linear(256, 10)
 
-print('Training Data Size: ' ,x_train_mnist.size(), '-', y_train_mnist.size())
-print('Testing Data Size: ' ,x_test_mnist.size(), '-', y_test_mnist.size())
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(x.size(0), -1) # Flatten layer
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.softmax(x)
 
-plt.imshow(x_train_mnist[0].numpy(), cmap='gray')
-plt.title('DIGIT: %i' % y_train_mnist[0])
-plt.show()
+model = Net().cuda()
+
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data).cuda(), Variable(target).cuda()
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target.squeeze())
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+
+for epoch in range(100):
+    train(epoch)
--- train_1.py
+++ train_1.py
@@ -5,108 +5,266 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 import torch.nn.functional as F
-from torchvision import transforms, models
+from torchvision import transforms
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from PIL import Image
 import copy
 
-train = pd.read_csv('train.csv')
-Y_train = train.ix[:, 0].values.astype('int32')
-X_train = (train.ix[:, 1:].values).astype('float32')
-X_test = (pd.read_csv('test.csv').values).astype('float32')
-
-scale = np.max(X_train)
-X_train /= scale
-X_test /= scale
-
-mean = np.std(X_train)
-X_train -= mean
-X_test -= mean
-
-X_train = X_train.reshape(42000, 28, 28)
-Y_train = np.array(Y_train, dtype=np.int64)
-
-X_train = torch.FloatTensor(X_train)
-Y_train = torch.LongTensor(Y_train)
+TRAIN_DATA = 'train.csv'
 
 
-def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=30):
-    lr = init_lr * (0.1**(epoch//lr_decay_epoch))
+class KaggleDataset(Dataset):
+    """Dataset wrapping images and target labels for Kaggle.
 
-    if epoch % lr_decay_epoch == 0:
-        print('LR is set to {}'.format(lr))
+    Arguments:
+        A CSV file path
+        Path to image folder
+        Extension of images
+        PIL transforms
+    """
 
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+    def __init__(self, csv_path, transform=None, mode=0, valid_size=1000):
+        self.mode = mode
+        tmp_df = pd.read_csv(csv_path)
 
-    return optimizer
+        self.transform = transform
+        if self.mode == 0: # train
+            self.X_train = (tmp_df.ix[:, 1:].values).astype('float32')[valid_size:]
+            self.X_train = self.X_train.reshape(len(self.X_train), 28, 28).astype('uint8')
+            self.y_train = tmp_df.ix[:, 0].values.astype('int64')[valid_size:].reshape(len(self.X_train), 1)
+        elif self.mode == 1: # val
+            self.X_train = (tmp_df.ix[:, 1:].values).astype('float32')[:valid_size]
+            self.X_train = self.X_train.reshape(len(self.X_train), 28, 28).astype('uint8')
+            self.y_train = tmp_df.ix[:, 0].values.astype('int64')[:valid_size].reshape(len(self.X_train), 1)
+        else: # test
+            self.X_train = (tmp_df.values).astype('float32')
+            self.X_train = self.X_train.reshape(len(self.X_train), 28, 28).astype('uint8')
+
+    def __getitem__(self, index):
+        img = Image.fromarray(self.X_train[index])
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.mode == 2:
+            return img
+        else:
+            label = torch.from_numpy(self.y_train[index])
+            return img, label
+
+    def __len__(self):
+        return len(self.X_train)
+
+
+train_transformations = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.1309], std=[0.3084])
+])
+
+valid_transformations = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.1309], std=[0.3084])
+])
+
+dset_train = KaggleDataset(TRAIN_DATA, train_transformations)
+dset_valid = KaggleDataset(TRAIN_DATA, valid_transformations, mode=1, valid_size=1000)
+dset_test = KaggleDataset('test.csv', valid_transformations, mode=2)
+
+train_loader = DataLoader(dset_train,
+                          batch_size=128,
+                          shuffle=True,
+                          num_workers=4)
+
+valid_loader = DataLoader(dset_valid,
+                          batch_size=128,
+                          shuffle=False,
+                          num_workers=1)
+
+test_loader = DataLoader(dset_test,
+                         batch_size=100,
+                         shuffle=False,
+                         num_workers=4)
 
 
 def save_checkpoint(state, filename='layer.pth.tar'):
     torch.save(state, filename)
 
+'''
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        self.batch_norm1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.batch_norm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.batch_norm3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.batch_norm4 = nn.BatchNorm2d(128)
+
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.batch_norm5 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.batch_norm6 = nn.BatchNorm2d(256)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.batch_norm7 = nn.BatchNorm2d(256)
+
+        self.avgpool = nn.AvgPool2d(7)
+        self.fc = nn.Linear(256, 10)
+
+    def forward(self, x):
+        # 28*28
+        x = F.relu(self.batch_norm1(self.conv1(x)))
+        x = F.max_pool2d(F.relu(self.batch_norm2(self.conv2(x))), 2)
+        # 14*14
+        x = F.relu(self.batch_norm3(self.conv3(x)))
+        x = F.max_pool2d(F.relu(self.batch_norm4(self.conv4(x))), 2)
+        # 7*7
+        x = F.relu(self.batch_norm5(self.conv5(x)))
+        x = F.relu(self.batch_norm6(self.conv6(x)))
+        x = F.relu(self.batch_norm7(self.conv7(x)))
+
+        # 1*1
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+'''
+
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+        self.batch_norm1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.batch_norm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.batch_norm3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.batch_norm4 = nn.BatchNorm2d(128)
 
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.batch_norm5 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.batch_norm6 = nn.BatchNorm2d(256)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.batch_norm7 = nn.BatchNorm2d(256)
 
-        self.flatten = nn.Linear(64*7*7, 256)
+        self.avgpool = nn.AvgPool2d(7)
         self.fc = nn.Linear(256, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = F.dropout(x, p=0.25, training=self.training)
+        # 28*28
+        x = F.relu(self.batch_norm1(self.conv1(x)))
+        x = F.max_pool2d(F.relu(self.batch_norm2(self.conv2(x))), 2)
+        # 14*14
+        x = F.relu(self.batch_norm3(self.conv3(x)))
+        x = F.max_pool2d(F.relu(self.batch_norm4(self.conv4(x))), 2)
+        # 7*7
+        x = F.relu(self.batch_norm5(self.conv5(x)))
+        x = F.relu(self.batch_norm6(self.conv6(x)))
+        x = F.relu(self.batch_norm7(self.conv7(x)))
 
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(F.relu(self.conv4(x)), 2, stride=2)
-        x = F.dropout(x, p=0.25, training=self.training)
+        # 1*1
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return F.log_softmax(x)
 
-        x = x.view(-1, 64*7*7)
-        x = F.relu(self.flatten(x))
-        x = F.dropout(x, p=0.5, training=self.training)
-        x = F.softmax(self.fc(x))
-        return x
+model = Net().cuda()
+
+criterion = nn.CrossEntropyLoss()
+optimizer_conv = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
 
-def train_model(model, criterion, optimizer, lr_scheduler, num_epochs):
-    best_model = model
+def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=20):
+    lr = init_lr * (0.1**(epoch//lr_decay_epoch))
+    if lr >= 0.00001:
+        if epoch % lr_decay_epoch == 0:
+            print('LR is set to {}'.format(lr))
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+    return optimizer
+
+
+def train(_model, _epoch, optimizer, lr_scheduler):
+    best_model = _model
     best_acc = 0.0
-
-    for epoch in range(1000):
-        optimizer = lr_scheduler(optimizer, epoch)
+    for epoch in range(_epoch):
         running_loss = 0.0
         running_corrects = 0.0
         val_corrects = 0.0
         val_loss = 0.0
         train_len = 0
         val_len = 0
-        model.training = True
 
+        model.train(True)
+        optimizer = lr_scheduler(optimizer, epoch)
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = Variable(data).cuda(), Variable(target).squeeze().cuda()
+            # plt.imshow(data.data.cpu().numpy()[0][0])
+            # plt.show()
+            optimizer.zero_grad()
+            output = _model(data)
+            _, preds = torch.max(output.data, 1)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
 
-        model.training = False
+            running_loss += loss.data[0]
+            running_corrects += torch.sum(preds == target.data)
+            train_len += data.size()[0]
 
-        print('[%d] loss: %f train_acc: %.4f val_loss: %f val_acc: %.4f' %
-              (epoch + 1, running_loss / 41800, running_corrects / 41800, val_loss, val_acc))
-        '''
+        model.train(False)
+        for batch_idx, (data, target) in enumerate(valid_loader):
+            data, target = Variable(data).cuda(), Variable(target).squeeze().cuda()
+            output = _model(data)
+            _, preds = torch.max(output.data, 1)
+            loss = criterion(output, target)
+
+            val_loss += loss.data[0]
+            val_corrects += torch.sum(preds == target.data)
+            val_len += data.size()[0]
+
+        val_loss = val_loss/val_len
+        val_acc = val_corrects/val_len
+
+        print('[%d] train_loss: %f train_acc: %.4f, val_loss: %f val_acc: %.4f' %
+              (epoch + 1, running_loss/train_len, running_corrects/train_len,
+               val_loss, val_acc))
+
         if val_acc >= best_acc:
             best_acc = val_acc
-            best_model = copy.deepcopy(model)
+            best_model = copy.deepcopy(_model)
             save_checkpoint(best_model, filename=str(epoch) + '.pth.tar')
-        '''
-    print('Finished Training')
+
     return best_model
 
-model = Net().cuda()
 
-criterion = nn.CrossEntropyLoss()
-optimizer_conv = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+# b_model = train(model, 100, optimizer_conv, exp_lr_scheduler)
+# save_checkpoint(b_model)
 
-best = train_model(model, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=500)
 
-save_checkpoint(best)
+def write_preds(preds, fname):
+    pd.DataFrame({"ImageId": list(range(1,len(preds)+1)), "Label": preds}).to_csv(fname, index=False, header=True)
+
+test_model = torch.load("10.pth.tar").cuda()
+test_model.train(False)
+test_len = 0
+predictions = np.zeros(28000, dtype=np.int64)
+
+for batch_idx, data in enumerate(test_loader):
+    data = Variable(data).cuda()
+    output = test_model(data)
+    _, preds = torch.max(output.data, 1)
+    preds = preds.cpu().numpy()
+    preds = preds.squeeze()
+    predictions[test_len:test_len+data.size()[0]] = preds
+    test_len += data.size()[0]
+
+write_preds(predictions, "result.csv")
+
